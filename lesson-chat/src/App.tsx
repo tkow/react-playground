85b68f61aae7081eecdd4e387a1055393c783c5d
tkow <t.kusama0613@outlook.jp>
2021-04-22 12:50:28 +0900

    feat: props explanation

--- lesson-chat/src/App.tsx
+++ lesson-chat/src/App.tsx
@@ -2,23 +2,31 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface WidgetProps {
+  message: string;
+}
+
+function Widget(props: WidgetProps) {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{props.message}</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Widget message="Hello React!" />
     </div>
   );
 }
